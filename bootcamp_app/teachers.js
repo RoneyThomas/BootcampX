67b148cbb027ae4a5ef1a16cb708558d2c571d0a
RoneyThomas <roneythomas6@gmail.com>
2022-01-06 05:41:59 -0500

    Fixed SQL Injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,18 +7,24 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher,
   cohorts.name as cohort
 FROM assistance_requests
   JOIN students ON students.id = assistance_requests.student_id
   JOIN teachers ON teachers.id = assistance_requests.teacher_id
   JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name LIKE '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name LIKE $1
 GROUP BY teachers.name,
   cohorts.name
 ORDER BY teacher;
-  `)
+  `;
+
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`];
+
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(teachers => {
       console.log(`${teachers.cohort}: ${teachers.teacher}`);
